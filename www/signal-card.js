7f4d6b1a073be1e36298d842300f532af57c7932
Sam Soffes <sam@soff.es>
2022-11-27 20:54:41 -0800

    Style signal card

--- www/signal-card.js
+++ www/signal-card.js
@@ -1,37 +1,109 @@
 class SignalCard extends HTMLElement {
-  // Whenever the state changes, a new `hass` object is set. Use this to
-  // update your content.
   set hass(hass) {
-    // Initialize the content if it's not there yet.
     if (!this.content) {
       this.innerHTML = `
         <ha-card>
           <div class="card-content"></div>
         </ha-card>
+        <style>
+          .card-content {
+            display: flex;
+            justify-content: space-between;
+
+            --connected-color: rgb(67, 160, 71);
+          }
+
+          .indicator {
+            display: inline-block;
+            width: 12px;
+            height: 12px;
+            border-radius: 6px;
+            background: var(--primary-background-color);
+            margin-right: 6px;
+          }
+
+          .indicator.indicator-connected {
+            background: var(--connected-color);
+          }
+
+          .carrier {
+            font-weight: 500;
+          }
+
+          .network {
+            color: var(--secondary-text-color);
+          }
+
+          .bars {
+            height: 16px;
+            display: flex;
+            align-items: end;
+            gap: 2px;
+          }
+
+          .bars div {
+            background: var(--primary-background-color);
+            width: 6px;
+          }
+
+          .bars div:nth-child(1) {
+            height: 20%;
+          }
+
+          .bars div:nth-child(2) {
+            height: 40%;
+          }
+
+          .bars div:nth-child(3) {
+            height: 60%;
+          }
+
+          .bars div:nth-child(4) {
+            height: 80%;
+          }
+
+          .bars div:nth-child(5) {
+            height: 100%;
+          }
+
+          .bars.bars-1 div:nth-child(1),
+          .bars.bars-2 div:nth-child(1), .bars.bars-2 div:nth-child(2),
+          .bars.bars-3 div:nth-child(1), .bars.bars-3 div:nth-child(2), .bars.bars-3 div:nth-child(3),
+          .bars.bars-4 div:nth-child(1), .bars.bars-4 div:nth-child(2), .bars.bars-4 div:nth-child(3), .bars.bars-4 div:nth-child(4),
+          .bars.bars-5 div:nth-child(1), .bars.bars-5 div:nth-child(2), .bars.bars-5 div:nth-child(3), .bars.bars-5 div:nth-child(4), .bars.bars-5 div:nth-child(5)
+          {
+            background-color: rgb(3, 155, 229);
+          }
+        </style>
       `;
       this.content = this.querySelector('div');
     }
 
-    const entityId = this.config.entity;
-    const state = hass.states[entityId];
-    const stateStr = state ? state.state : 'unavailable';
+    const signal = hass.states['sensor.peplink_signal'];
+    const carrier = hass.states['sensor.peplink_carrier'];
+    const network = hass.states['sensor.peplink_network'];
+
+    if (!signal || !carrier || !network) {
+      this.content.innerHTML = '<div class="indicator"></div> Unknown';
+      return;
+    }
 
     this.content.innerHTML = `
-      Signal: ${stateStr} of 5
+      <div class="status">
+        <div class="indicator indicator-connected"></div>
+        <span class="carrier">${carrier.state}</span>
+        <span class="network">${network.state}</span>
+      </div>
+      <div class="bars bars-${signal.state}">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>
     `;
   }
 
-  // The user supplied configuration. Throw an exception and Home Assistant
-  // will render an error card.
   setConfig(config) {
-    if (!config.entity) {
-      throw new Error('You need to define an entity');
-    }
     this.config = config;
   }
 
-  // The height of your card. Home Assistant uses this to automatically
-  // distribute all cards over the available columns.
   getCardSize() {
     return 1;
   }
